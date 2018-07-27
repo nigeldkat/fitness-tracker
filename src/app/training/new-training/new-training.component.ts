71a43fb76f68e8936d1a491c4b6003641642bebc
nigeldkat <nigeldkat@hotmail.com>
2018-07-27 15:55:04 -0400

    training service wire up

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm} from '@angular/forms';  //for two way binding or template form
+
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
  
@@ -9,8 +11,6 @@ import { Exercise } from '../exercise.model';
 })
 export class NewTrainingComponent implements OnInit {
 
-  @Output() trainingStart = new EventEmitter<void>();
-
   exercises: Exercise[] = [];
 
   constructor( private trainingService: TrainingService) { }
@@ -19,8 +19,8 @@ export class NewTrainingComponent implements OnInit {
     this.exercises = this.trainingService.getAvailableExercises();
   }
 
-  onStartTraining(){
-    this.trainingStart.emit();
+  onStartTraining(form: NgForm){
+    this.trainingService.startExercise(form.value.exercise);
   }
 
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,6 +1,9 @@
+import {Subject} from 'rxjs/Subject';
+
 import { Exercise } from './exercise.model';
 
 export class TrainingService {
+    exerciseChanged = new Subject<Exercise>();
     private availableExercises: Exercise[] = [
         { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
         { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -8,7 +11,14 @@ export class TrainingService {
         { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
     ];
 
+    private runningExercise: Exercise;
+
     public getAvailableExercises(){
         return this.availableExercises.slice(); //this creates a copy
+    }
+
+    startExercise(selectedId: string){
+        this.runningExercise = this.availableExercises.find(ex => ex.id == selectedId);
+        this.exerciseChanged.next({...this.runningExercise});
     }
 }
