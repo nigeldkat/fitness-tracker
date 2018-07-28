690033d20ea0075b74a96f031d4caf1e5f9266ab
nigeldkat <nigeldkat@hotmail.com>
2018-07-28 15:45:45 -0400

    added data table

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -48,4 +48,9 @@ export class TrainingService {
     getRunningExercise() {
         return { ...this.runningExercise };
     }
+
+    getCompletedOrCancelledExercises(){
+        return this.exercises.slice();
+    }
+
 }
