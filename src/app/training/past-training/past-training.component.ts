b6d31cbc84735f27f0e646830ab1d198cd9c2945
nigeldkat <nigeldkat@hotmail.com>
2018-07-29 09:52:31 -0400

    sort, fitler and pageing in grid

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource} from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit, ViewChildren } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
 import { Exercise} from '../exercise.model';
 import { TrainingService } from '../training.service';
 
@@ -8,14 +8,26 @@ import { TrainingService } from '../training.service';
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns =['date','name','duration','calories','state'];
   dataSource = new MatTableDataSource<Exercise>();
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private trainingSource:TrainingService) { }
 
   ngOnInit() {
     this.dataSource.data = this.trainingSource.getCompletedOrCancelledExercises();
+  }
+
+  ngAfterViewInit(){
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(filterValue: string){
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
   }
 
 }
