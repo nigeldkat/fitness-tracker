b30d39f1f26eca160961397d1f65a335605a4bc6
nigeldkat <nigeldkat@hotmail.com>
2018-07-29 17:24:56 -0400

    mapping obserable

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';  //for two way binding or template form
 
 import { AngularFirestore } from 'angularfire2/firestore';
-//import { Obserable } from 'rxjs'
-//import { Subject } from 'rxjs/Subject';
-//import { Obserable } from 'rxjs';
+
 
 
 import { Observable, Subscription } from 'rxjs';
@@ -19,28 +17,69 @@ import { Exercise } from '../exercise.model';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-//for initial
+  //for initial
   //exercises: Exercise[] = [];
-  exercises: Observable<any>;
-  
+  //exercises: Observable<any>; -- for valuechanges
+  exercises: Observable<Exercise[]>;
 
-//, private db: AngularFirestore
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
 
-    //without subscription
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    //without subscription using valuechanges
+    //this.exercises = this.db.collection('availableExercises').valueChanges();
 
-//let x = this.db.collection('availableExercises').valueChanges();
-
+    this.exercises =
+      this.db.collection('availableExercises')
+        .snapshotChanges()
+        .map(docArray => {
+          return docArray.map(doc => {
+            return {
+              id: doc.payload.doc.id,
+              name: doc.payload.doc.data().name,
+              duration: doc.payload.doc.data().duration,
+              calories: doc.payload.doc.data().calories
+            };
+          });
+        });
 
 
     //valueChanges strips off meta data
-    this.db.collection('availableExercises').valueChanges()
-    .subscribe(result =>{
-      console.log('my results', result)
-    })
+    // this.db.collection('availableExercises').valueChanges()
+    // .subscribe(result =>{
+    //   console.log('my results', result)
+    // })
+
+    // //sanpshotChanges gets meta data but no data
+    // this.db.collection('availableExercises').snapshotChanges()
+    // .subscribe(result =>{
+    //   console.log('my results', result)
+    // })
+
+    //sanpshotChanges using payload
+    // this.db.collection('availableExercises').snapshotChanges()
+    // .subscribe(result =>{
+    //   for (const res of result){
+    //     console.log('my res.payload.data ', res.payload.doc.data())
+    //   }
+
+    // })
+
+    // //mapping to doc object
+    // this.db.collection('availableExercises')
+    // .snapshotChanges()
+    // .map( docArray => {
+    //   return docArray.map( doc =>{
+    //     return {
+    //       id: doc.payload.doc.id,
+    //       ...doc.payload.doc.data()
+    //     };
+    //   });
+    // })
+    // .subscribe(result =>{
+    //   console.log('my results', result);      
+    // })
+
     //old
     //this.exercises = this.trainingService.getAvailableExercises();
   }
