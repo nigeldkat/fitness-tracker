1dba61d6a5f8e5d8d5c53e46084a034c163646ca
nigeldkat <nigeldkat@hotmail.com>
2018-08-04 13:18:04 -0400

    create common shared module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,17 @@
 
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+//import { CommonModule } from '@angular/common';
+//import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
-import { MaterialModule } from '../material.module';
-import { FlexLayoutModule} from '@angular/flex-layout';
+//import { MaterialModule } from '../material.module';
+//import { FlexLayoutModule} from '@angular/flex-layout';
 
 import { LoginComponent} from './login/login.component';
 import { SignupComponent} from './signup/signup.component';
+
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -17,12 +19,13 @@ import { SignupComponent} from './signup/signup.component';
         SignupComponent
     ],
     imports: [
-        CommonModule,
-        FormsModule, 
-        ReactiveFormsModule,
-        MaterialModule, 
-        FlexLayoutModule,
+        //CommonModule,
+        // FormsModule, 
+        // ReactiveFormsModule,
+        // MaterialModule, 
+        // FlexLayoutModule,
         AngularFireAuthModule,
+        SharedModule
     ],
     exports: [
 
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,12 +1,14 @@
 
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule} from '@angular/forms'; //, ReactiveFormsModule
+//import { CommonModule } from '@angular/common';
+//import { FormsModule} from '@angular/forms'; //, ReactiveFormsModule
 
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
-import { MaterialModule } from '../material.module';
-import { FlexLayoutModule} from '@angular/flex-layout';
+//import { MaterialModule } from '../material.module';
+//import { FlexLayoutModule} from '@angular/flex-layout';
+
+import { SharedModule } from '../shared/shared.module';
 
 import { CurrentTrainingComponent } from './current-training/current-training.component'
 import { NewTrainingComponent } from './new-training/new-training.component';
@@ -23,12 +25,13 @@ import { TrainingComponent} from './training.component';
         TrainingComponent
     ],
     imports: [
-        CommonModule,
-        FormsModule, 
+        //CommonModule,
+        //FormsModule, 
         // ReactiveFormsModule,
-        MaterialModule, 
-        FlexLayoutModule,
-        AngularFirestoreModule
+        //MaterialModule, 
+        //FlexLayoutModule,
+        AngularFirestoreModule,
+        SharedModule
     ],
     exports: [
     ],
